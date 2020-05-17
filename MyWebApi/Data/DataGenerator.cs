20eebec8a7dba91dc53b2232fc1fd1ed7798bc93
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-05-18 02:01:57 +0430

    Add project files.

---
+++ MyWebApi/Data/DataGenerator.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MyWebApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace MyWebApi.Data
+{
+    public class DataGenerator
+    {
+        public static void Initialize(IServiceProvider serviceProvider)
+        {
+            using (var context = new AppDbContext(
+                serviceProvider.GetRequiredService<DbContextOptions<AppDbContext>>()))
+            {
+                if (context.Student.Any())
+                {
+                    return;
+                }
+
+                context.Student.AddRange(
+                    new Student()
+                    {
+                        Id = 1,
+                        Age = 22,
+                        FirstName = "Masoud",
+                        LastName = "Khodadadi",
+                        StdNumber = "954421016"
+                    },
+                    new Student()
+                    {
+                        Id = 2,
+                        Age = 21,
+                        FirstName = "Javad",
+                        LastName = "Javadi",
+                        StdNumber = "954421017"
+                    },
+                    new Student()
+                    {
+                        Id = 3,
+                        Age = 23,
+                        FirstName = "Sirvan",
+                        LastName = "Khosravi",
+                        StdNumber = "954421018"
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
---
+++ MyWebApi/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyWebApi.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string StdNumber { get; set; }
+        public int Age { get; set; }
+    }
+}
