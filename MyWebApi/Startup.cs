29af61752c81b09bc3c69ca6786351d47f7ad3ce
Masoud Khodadadi <masoud.xpress@gmail.com>
2020-06-03 03:08:40 +0430

    Add swagger ui

--- MyWebApi/Startup.cs
+++ MyWebApi/Startup.cs
@@ -11,6 +11,8 @@ using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.EntityFrameworkCore;
 using MyWebApi.Data;
+using System.Text.Json.Serialization;
+using Microsoft.OpenApi.Models;
 
 namespace MyWebApi
 {
@@ -25,10 +27,22 @@ namespace MyWebApi
 
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddControllers()
+                .AddJsonOptions(options =>
+                    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter())); ;
 
             services.AddDbContext<AppDbContext>(options =>
                     options.UseInMemoryDatabase(databaseName: "MyWebApi"));
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Title = "University - Student management HTTP API",
+                    Version = "v1",
+                    Description = "The Student Management Microservice HTTP API. This is a Data-Driven/CRUD microservice"
+                });
+            });
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -46,6 +60,12 @@ namespace MyWebApi
             {
                 endpoints.MapControllers();
             });
+
+            app.UseSwagger()
+                .UseSwaggerUI(c =>
+                {
+                    c.SwaggerEndpoint("/swagger/v1/swagger.json", "Course API V1");
+                });
         }
     }
 }
